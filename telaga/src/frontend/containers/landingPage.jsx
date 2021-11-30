559d0678dd1c33bf565a38676318b8ff975ef7ee
MorningToast21 <Kevinnp28@gmail.com>
2021-11-30 12:03:06 +0700

    pp3-initial
    - Create framework for frontend development

---
+++ telaga/src/frontend/containers/landingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Navbar} from '../components/navbar'
+
+const landingPage = () => {
+    return (
+        <div>
+            <div className="header">
+                <h1> Landing Page </h1>
+            </div>
+        </div>
+    )
+}
+
+export default landingPage
